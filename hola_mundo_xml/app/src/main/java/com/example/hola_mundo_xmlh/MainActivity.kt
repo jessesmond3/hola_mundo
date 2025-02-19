40ce61d92c92738848053b5f9dc8dc2cb95e01f3
Sareh <al213178@alumnos.uacj.mx>
2025-02-19 08:38:37 -0700

    [FINALIZAR]

--- hola_mundo_xml/app/src/main/java/com/example/hola_mundo_xmlh/MainActivity.kt
+++ hola_mundo_xml/app/src/main/java/com/example/hola_mundo_xmlh/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.hola_mundo_xmlh
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var boton_pulsar: Button
+    lateinit var cajon_de_texto: TextView
+    lateinit var cajon_de_texto_a_introducir: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        cajon_de_texto = findViewById(R.id.cajon_texto_vista)
+        boton_pulsar = findViewById(R.id.boton_magico)
+        cajon_de_texto_a_introducir = findViewById(R.id.entrada_de_texto)
+
+        boton_pulsar.setOnClickListener {
+            mandar_texto_al_cajon_de_vista_texto()
+        }
+    }
+
+    fun mandar_texto_al_cajon_de_vista_texto(){
+        cajon_de_texto.text = cajon_de_texto_a_introducir.text
+        cajon_de_texto_a_introducir.setText("")
     }
 }
